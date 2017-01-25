5f0fef8f8f85b01338d10b9c93046716b4de542b
johannhawk <joichanse@live.com>
2017-01-25 11:26:18 +0000

    Valmynd v2 og daemi 1

--- git_verkefni.py
+++ git_verkefni.py
@@ -1,6 +1,25 @@
+val = 4
+while val != 0:
+    print("=+=Valmynd=+=")
+    print("1.")
+    print("?.")
+    print("?.")
+    print("0. Hætta")
+    print("=+=Valmynd=+=")
+    val = int(input("Veldu :"))
+    validerror = 1
 
-print("=+=Valmynd=+=")
-print("1.")
-print("?.")
-print("?.")
-print("=+=Valmynd=+=")
+    if val == 1:
+        print("=1=")
+        num11 = int(input("Sláðu inn númer : "))
+        num12 = int(input("Sláðu inn næsta númerið : "))
+        num13 = num11*num12
+        num14 = num11+num12
+        print("Samanloögð =",num14)
+        print("Margfölduð =",num13)
+        validerror = 0
+
+
+
+    if validerror == 1:
+        print("!!! Ekki rétt númer !!!")
